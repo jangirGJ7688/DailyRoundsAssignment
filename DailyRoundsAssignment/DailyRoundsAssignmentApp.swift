4f11a4efb95825ef3c35669bdd1afed532c4a7de
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2025-04-09 18:23:19 +0530

    Initial Commit

---
+++ DailyRoundsAssignment/DailyRoundsAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRoundsAssignmentApp.swift
+//  DailyRoundsAssignment
+//
+//  Created by Ganpat Jangir on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyRoundsAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
